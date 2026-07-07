7bc1203a27ffde5241d9558533a18c1d9d79bf40
HellRiqueyy <rique.silva.moura@gmail.com>
2026-07-07 20:25:32 -0300

    Refatoração da api | 07/07/2026

--- api/conexao.php
+++ api/conexao.php
@@ -1,15 +1,16 @@
 <?php
-//Credenciais do banco/API
-$host = "127.0.0.1";
-$usuario = "root";
-$senha = "";
-$banco = "db_estagiou";
+// Credenciais do banco/API
+$host = getenv('DB_HOST') ?: '127.0.0.1';
+$usuario = getenv('DB_USER') ?: 'root';
+$senha = getenv('DB_PASS') ?: '';
+$banco = getenv('DB_NAME') ?: 'db_estagiou';
+$porta = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($host, $usuario, $senha, $banco);
+$conn = new mysqli($host, $usuario, $senha, $banco, $porta);
 
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
 }
 
-$conn->set_charset("utf8");
+$conn->set_charset('utf8');
 ?>
