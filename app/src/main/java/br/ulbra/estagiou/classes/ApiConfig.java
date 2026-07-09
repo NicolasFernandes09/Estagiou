69557d1a2ec17b42c4a4fafd638e16f7788b7e19
Filipe Gabriel <146496600+Fg2007@users.noreply.github.com>
2026-07-09 19:58:29 -0300

    Atualiza IP da API

--- app/src/main/java/br/ulbra/estagiou/classes/ApiConfig.java
+++ app/src/main/java/br/ulbra/estagiou/classes/ApiConfig.java
@@ -1,7 +1,7 @@
 package br.ulbra.estagiou.classes;
 
 public class ApiConfig {
-    public static final String BASE_URL = "http://172.29.20.89/Estagiou/api/";
+    public static final String BASE_URL = "http://172.29.20.149/Estagiou/api/";
     public static final String VAGAS_URL = BASE_URL + "vagas.php";
     public static final String USUARIOS_URL = BASE_URL + "usuarios.php";
 }
