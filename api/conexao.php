b46f0ad140cb5507f130564c1ec0dc7e13ff542b
HellRiqueyy <rique.silva.moura@gmail.com>
2026-07-08 21:07:36 -0300

    Criação da api usuarios

--- api/conexao.php
+++ api/conexao.php
@@ -1,11 +1,11 @@
 <?php
 //Credenciais do banco/API
-$host = "172.29.20.169";
+$host = "172.29.20.89";
 $usuario = "root";
 $senha = "";
 $banco = "db_estagiou";
 
-$conn = new mysqli($host, $usuario, $senha, $banco, $porta);
+$conn = new mysqli($host, $usuario, $senha, $banco);
 
 if ($conn->connect_error) {
     die("Erro na conexão: " . $conn->connect_error);
